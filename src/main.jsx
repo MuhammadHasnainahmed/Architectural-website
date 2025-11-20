6d99d0d25e0bbd2de7fd23b7a089c054a92e567c
= <=>
2025-11-20 17:09:50 +0500

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Navbar from './Component/Navbar.jsx'
-import Footer from './Component/Footer.jsx'
-import { BrowserRouter } from 'react-router-dom'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import Navbar from './Component/Navbar.jsx'
+// import Footer from './Component/Footer.jsx'
+// import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode >
+// createRoot(document.getElementById('root')).render(
+//   // <StrictMode >
+//     <BrowserRouter>
+//     <Navbar  />
+//     <App />
+//     <Footer/>
+//     </BrowserRouter>
+//   // </StrictMode>
+// )
+
+
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Navbar from "./Component/Navbar.jsx";
+import Footer from "./Component/Footer.jsx";
+import { BrowserRouter } from "react-router-dom";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <BrowserRouter>
-    <Navbar  />
-    <App />
-    <Footer/>
+      {/* Navbar stays outside Routes */}
+      <Navbar />
+      {/* App contains all your Routes */}
+      <App />
+      {/* Footer stays outside Routes */}
+      <Footer />
     </BrowserRouter>
   </StrictMode>
-)
+);
